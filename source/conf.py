d81a8221f133c0849d59434f534dc358e05af1a0
petruatanasiu <petru.atanasiu@gmail.com>
2023-10-17 19:37:27 +0300

    Activate theme on doc ops

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@ release = 'v1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import sphinx_rtd_theme
+
 extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.duration",
@@ -32,5 +34,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
